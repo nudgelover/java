c343555bee93416f84875c17414ae3d5a84e07b7
nudgelover <124110414+nudgelover@users.noreply.github.com>
2023-02-23 17:09:16 +0900

    Update P68.java
    test 수정

--- day02/src/variable/P68.java
+++ day02/src/variable/P68.java
@@ -5,15 +5,15 @@ import java.util.Random;
 public class P68 {
 
 	public static void main(String[] args) {
-		String s1 = "10";
+		String s1 = "11";
 		//string을 int로 바꿀 때
 		int s2 = Integer.parseInt(s1);
 		int a = 10;
 		System.out.println(s2+a);
 		
 	
-		int n1 =10;
-		//int를 string으로 바꿀 때
+		int n1 = 10;
+		//int를 string으로 바꿀 때 
 		//String n2 = n1+"";
 		String n2 = String.valueOf(n1);
 		
