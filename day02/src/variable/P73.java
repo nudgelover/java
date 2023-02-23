b884e5ff0d10c3d4e89ffc23e56d80bac9a0344a
nudgelover <wlsgml2422@gmail.com>
2023-02-23 17:41:05 +0900

    수정

--- day02/src/variable/P73.java
+++ day02/src/variable/P73.java
@@ -7,7 +7,7 @@ public class P73 {
 	public static void main(String[] args) {
 		int a = 10; //깃연습용
 		Scanner sc = new Scanner(System.in);
-		System.out.println("명령어를 입력하시오(q,i,d,y)");
+		System.out.println("명령어를 입력하시오(q,i,d,s)");
 		String cmd = sc.next();
 		System.out.printf("%s를 입력했습니다.", cmd);
 		sc.close();
