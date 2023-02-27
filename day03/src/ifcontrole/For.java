a9f4510637524c5da22cf9a15db72c8021c3f01d
nudgelover <wlsgml2422@gmail.com>
2023-02-27 10:13:55 +0900

    test

--- day03/src/ifcontrole/For.java
+++ day03/src/ifcontrole/For.java
@@ -4,11 +4,15 @@ public class For {
 
 	public static void main(String[] args) {
 
-		for (int i = 2; i < 10; i++) {
+		myfor: for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
-				System.out.printf("%d * %d = %d \n", i, j, (i * j));
+				System.out.printf("%d * %d = %d\n", i, j, (i * j));
+				if ((i * j) == 21) {
+					break myfor;
+				}
 			}
+			System.out.println("");
 		}
 	}
 }
--- day03/src/whilecontrol/P96.java
+++ day03/src/whilecontrol/P96.java
@@ -3,8 +3,7 @@ package whilecontrol;
 public class P96 {
 
 	public static void main(String[] args) {
-		int cc = 10000;
-		int ccccc = 100;
+
 		// 짝수의 합을 구하시오
 		System.out.println("start...");
 
