777b2f451e25958b830a7c8bb7982debc85ab772
nudgelover <wlsgml2422@gmail.com>
2023-02-27 09:34:15 +0900

    test

--- day03/src/whilecontrol/P96.java
+++ day03/src/whilecontrol/P96.java
@@ -3,9 +3,8 @@ package whilecontrol;
 public class P96 {
 
 	public static void main(String[] args) {
-		int a = 1000;
+
 		// 짝수의 합을 구하시오
-		int bb = 2200;
 		System.out.println("start...");
 
 		int cnt = 0;
