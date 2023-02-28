b8ffd27d44bbde6b7b20d19845e7b396a0d26949
nudgelover <wlsgml2422@gmail.com>
2023-02-28 13:04:16 +0900

    230228

---
+++ day04/src/oop1/App.java
@@ -0,0 +1,18 @@
+package oop1;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 얘는 객체지향이랑 상관없음. 실행시켜주는 역할이기에 static void main 써야함
+		Car car1 = new Car();
+		car1.go();
+		car1.stop();
+		
+		Car car2 = new Car();
+		car2.go();
+		car2
+		.stop();
+
+	}
+
+}
---
+++ day04/src/oop1/App2.java
@@ -0,0 +1,17 @@
+package oop1;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		Car car1 = new Car("k2","blue",2000);
+		car1.go();
+		car1.stop();
+
+
+		Car car2 = new Car("k3", "white", 2000);
+		car2.go();
+		car2.stop();
+
+	}
+
+}
