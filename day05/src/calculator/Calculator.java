fa6e654830143eb096264050e9ae9d5a0ccf2fd5
nudgelover <wlsgml2422@gmail.com>
2023-03-03 10:36:00 +0900

    230303

---
+++ day05/src/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Calculator {
+	public int sum(int a, int b) {
+		int result = 0;
+		result = a+b;
+		return result;
+		//값이 이렇게 돌려보내야할 때는  return
+	}
+	
+	public void save(int a) {
+		//돌아올게 없을 때는 void! 값을 전달 받아서 처리만 해주면 되니.
+	}
+	
+}
